00fe0114518c7f62a7346ba65f4da7ceb1a4348b
Ivan Rep <ivan.rep2@gmail.com>
2018-04-13 01:12:20 +0200

    Public

--- Swift_Helpers/UIKit/Extensions/UIImage_FromColor.swift
+++ Swift_Helpers/UIKit/Extensions/UIImage_FromColor.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 extension UIImage {
-    static func from(color: UIColor, with size: CGSize) -> UIImage? {
+    public static func from(color: UIColor, with size: CGSize) -> UIImage? {
         let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
 
         UIGraphicsBeginImageContextWithOptions(size, false, 0)
