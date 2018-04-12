816f30bbc879844667dd4c4161b6a6ddcac6c72d
Ivan Rep <ivan.rep2@gmail.com>
2018-04-13 01:09:23 +0200

    Initial commit

---
+++ Swift_Helpers/Swift_Helpers.h
@@ -0,0 +1,19 @@
+//
+//  Swift_Helpers.h
+//  Swift_Helpers
+//
+//  Created by Ivan Rep on 13/04/2018.
+//  Copyright © 2018 Rep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swift_Helpers.
+FOUNDATION_EXPORT double Swift_HelpersVersionNumber;
+
+//! Project version string for Swift_Helpers.
+FOUNDATION_EXPORT const unsigned char Swift_HelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_Helpers/PublicHeader.h>
+
+
